e53cc1198db57217899f736122626b572861b37c
RVAwiress <41031016+RVAwiress@users.noreply.github.com>
2018-07-09 23:55:09 -0400

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,28 @@
 # makeCacheMatrix contains a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    # this function contains the get & set functions for the matrix data and the matrix inverse
+    # minv will be the matrix inverse, initially set to null since it's not cached yet
     minv <- NULL
+    
+    # the set function is called when new data is put into the matrix
+    # the new data is passed through to the variable x, and the matrix inverse is set to null since it hasn't been calculated yet for this new data
+    # these changes are made in the parent environment
     set <- function(y) {
         x <<- y
         minv <<- NULL
     }
+    
+    # the get function is called to retrieve the data in the matrix, so it just returns that data directly
     get <- function() x
+    
+    # set inverse has to solve for the inverse in order to set it, done in the parent environment
     setinverse <- function(solve) minv <<- solve
+    
+    # get inverse returns the inverse, which may have been set or it may be null
     getinverse <- function() minv
+    
+    # the function returns a list of the 4 nested functions, with their names for external access (e.g. makeCacheMatrix$set) equal to their names inside this function
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -21,15 +35,24 @@ makeCacheMatrix <- function(x = matrix()) {
 # cacheSolve computes the inverse of the special "matrix" object from makeCacheMatrix. If the inverse already exists, it is accessed instead of computed.
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+    # first get minv from the parent (or above) environment to see if it's been set
     minv <- x$getinverse()
+
+    # if it has been set, print that the data is cached and then return minv
     if(!is.null(minv)) {
         message("getting cached data")
-        return(minv)
+        return(minv) # rest of function is not processed
     }
+    
+    # otherwise minv is not set, so need to set it
+    # get the matrix data
     data <- x$get()
+    # calculate the inverse
     minv <- solve(data, ...)
+    # set the inverse in the parent (or above) environment
     x$setinverse(minv)
+    
+    # return the matrix inverse itself as the return for cacheSolve (in addition to setting it in its environment)
     minv
     
 }
